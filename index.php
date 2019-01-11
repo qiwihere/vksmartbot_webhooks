2bceeebb98c71ad21778e701b9f2173be85e9a34
qiwihere <cleoveloA1>
2019-01-11 15:19:00 +0300

    Update

--- index.php
+++ index.php
@@ -6,23 +6,9 @@ $pass = 'kek';
 if($login != $_SERVER['PHP_AUTH_USER'] or $pass != $_SERVER['PHP_AUTH_PW']) die();
 
 
-function processMessage($update) {
-    if($update["result"]["action"] == "weather"){
-        sendMessage(array(
-            "source" => $update["result"]["source"],
-            "speech" => "Hello from webhook",
-            "displayText" => "Hello from webhook",
-            "contextOut" => array()
-        ));
-    }
-}
+$webhook_data = json_decode(file_get_contents('php://input'),true);
+$action = $webhook_data['queryResult']['action'];
 
-function sendMessage($parameters) {
-    echo json_encode($parameters);
-}
-
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-if (isset($update["result"]["action"])) {
-    processMessage($update);
-}
+$answer=[
+    'method'=>$action
+];
