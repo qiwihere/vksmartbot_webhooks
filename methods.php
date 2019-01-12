55edc76158e51f930ea0f40156048a1f7875b9ef
Артем <qiwihere@gmail.com>
2019-01-12 05:18:47 +0300

    Update

--- methods.php
+++ methods.php
@@ -2,12 +2,29 @@
 
 function get_weather($city)
 {
+    //город в ИП
     $im_p = json_decode(
         file_get_contents('https://htmlweb.ru/service/api.php?inflect='.$city.'&json&partofspeech=С'),
         true
     );
+    $city = $im_p[0];
+
+    //код города
+    $xml_cities = file_get_contents('https://pogoda.yandex.ru/static/cities.xml');
+    $p = xml_parser_create();
+    xml_parse_into_struct($p, $xml_cities, $vals, $index);
+    xml_parser_free($p);
+
+    foreach($vals as $k=>$val)
+    {
+        if(strtolower($val['value'])==strtolower($city))
+        {
+            $city_id = $vals[$k]['attributes']['ID'];
+            break;
+        }
+    }
     $answer='{
-        "fulfillmentText": "'.$im_p[0].'",
+        "fulfillmentText": "'.$city_id.'",
         "source": "EchoService"
     }';
     echo($answer);
