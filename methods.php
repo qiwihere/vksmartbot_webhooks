036c57c3067f43e8a333964e89227449a2f9194d
Артем <qiwihere@gmail.com>
2019-01-12 04:13:18 +0300

    Update

--- methods.php
+++ methods.php
@@ -3,9 +3,9 @@
 function get_weather($city)
 {
     $str_answ = 'Ты внатуре хочешь знать погоду в '.$city;
-    $answer=[
-        'fulfillmentText'=>$str_answ,
-        'source'=>'EchoService'
-    ];
+    $answer='{
+        "fulfillmentText": "'.$str_answ.'"",
+        "source": "EchoService"
+    }';
     echo(json_encode($answer));
 }
