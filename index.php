e63ddd2f0d8df6cff8445511b073edfe2ec09082
Артем <qiwihere@gmail.com>
2019-01-12 18:10:44 +0300

    Update

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $login = 'test';
 $pass = 'kek';
 
 //auth
-if($login != $_SERVER['PHP_AUTH_USER'] or $pass != $_SERVER['PHP_AUTH_PW']) die();
+//if($login != $_SERVER['PHP_AUTH_USER'] or $pass != $_SERVER['PHP_AUTH_PW']) die();
 
 
 $webhook_data = json_decode(file_get_contents('php://input'),true);
