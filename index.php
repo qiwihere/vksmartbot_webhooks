bd149a55f2dc25fac087e2636f579622fea461c6
Артем <qiwihere@gmail.com>
2019-01-12 18:15:35 +0300

    Update

--- index.php
+++ index.php
@@ -5,17 +5,16 @@ $login = 'test';
 $pass = 'kek';
 
 //auth
-//if($login != $_SERVER['PHP_AUTH_USER'] or $pass != $_SERVER['PHP_AUTH_PW']) die();
+if($login != $_SERVER['PHP_AUTH_USER'] or $pass != $_SERVER['PHP_AUTH_PW']) die();
 
 
 $webhook_data = json_decode(file_get_contents('php://input'),true);
 $action = $webhook_data['queryResult']['action'];
 
-$action='weather';
+
 
 if($action=='weather')
 {
     $city = $webhook_data['queryResult']['parameters']['city'];
-    $city = $_GET['city'];
     get_weather($city);
 }
