50c735376bad116da18998f947f637d420ecc7a4
Артем <qiwihere@gmail.com>
2019-01-12 04:35:32 +0300

    Update

--- methods.php
+++ methods.php
@@ -2,9 +2,16 @@
 
 function get_weather($city)
 {
-    $im_p = json_decode(file_get_contents('https://htmlweb.ru/service/api.php?inflect='.$city.'&json'),true);
+    $im_p = json_decode(file_get_contents('https://htmlweb.ru/service/api.php?inflect='.$city.'&json&info'),true);
+    foreach($im_p as $word_p)
+    {
+        if ($word_p['partofspeech'] == "C") {
+            $word = $word_p['word'];
+            break;
+        }
+    }
     $answer='{
-        "fulfillmentText": "'.$im_p[0].'",
+        "fulfillmentText": "'.$word.'",
         "source": "EchoService"
     }';
     echo($answer);
